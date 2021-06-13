22f39afd95c5e29990cc4918ed2918092e583212
beyanasr <nasr5beya@gmail.com>
2021-06-13 20:24:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UserList from './components/userList/UsersList';
+
+
+function App() {
+  return (
+    <div className="App">
+  <UserList/>
+    </div>
+  );
+}
+
+export default App;
